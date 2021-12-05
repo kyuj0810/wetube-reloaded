41e94af2968186ff91bb1bb37d2f651ce9c92c8a
YuJin Kim <kyuj0810@gmail.com>
2021-12-05 23:03:38 +0900

    video upload

--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,48 +1,44 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2 minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  //const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  res.render("home", { pageTitle: "Home", videos });
+/* callback 형식
+console.log("start")
+ Video.find({}, (error, videos) => { 
+   if(error) {
+     return res.render("server-error")
+   }
+   return res.render("home", { pageTitle: "Home", videos });
+ });
+console.log("finished") 
+ */
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos });
+  // try {
+  //   console.log("start");
+  //   const videos = await Video.find({});
+  //   console.log(videos);
+  //   console.log("finished");
+  //   return res.render("home", { pageTitle: "Home", videos });
+  // } catch (error) {
+  //   return res.render("server-error", { error });
+  // }
+  /**
+   * callback 은 err와 docs 라는 signature 를 가짐.
+   */
 };
 export const watch = (req, res) => {
-  const { id } = req.params; // ES6 => const id = req.params.id;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  const { id } = req.params;
+  return res.render("watch", { pageTitle: `Watching` });
 };
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
 
@@ -50,18 +46,18 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
-  const newVideos = {
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
     title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
 
-  videos.push(newVideos);
-  // here we will add a video to the videos array.
   return res.redirect("/");
 };
