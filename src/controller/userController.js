a7e5d7d0fd5f494c7e87c67613f33b7099d78ade
YuJin Kim <kyuj0810@gmail.com>
2022-01-12 20:34:22 +0900

    git로그인 연동

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -64,6 +64,20 @@ export const postLogin = async (req, res) => {
   return res.redirect("/");
 };
 
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: "00570ff9168b76b384bb",
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  const params = new URLSearchParams(config);
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+
+export const finishGithubLogin = (req, res) => {};
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log Out");
