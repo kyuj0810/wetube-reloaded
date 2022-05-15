3dba31d8c477f404e485c6f005c9ded223db8062
YuJin Kim <kyuj0810@gmail.com>
2022-05-15 21:17:17 +0900

    8.4 SCSS Loader

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/client/js/main.js",
+  plugins: [new MiniCssExtractPlugin()],
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "assets", "js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
