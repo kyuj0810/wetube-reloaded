e53fb62a7ef0066fda2589ddb377ea98237f60af
YuJin Kim <kyuj0810@gmail.com>
2022-07-03 23:32:04 +0900

    css

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,7 +1,6 @@
-import bcrypt from "bcrypt";
-import fetch from "node-fetch";
 import User from "../models/User";
-import Video from "../models/Video";
+import fetch from "node-fetch";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -225,7 +224,13 @@ export const postChangePasswor = async (req, res) => {
 
 export const see = async (req, res) => {
   const { id } = req.params;
-  const user = await User.findById(id).populate("videos");
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  });
 
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
--- src/init.js
+++ src/init.js
@@ -4,7 +4,7 @@ import "./models/Video";
 import "./models/User";
 import app from "./server";
 
-const PORT = 5001;
+const PORT = 5002;
 
 const handleListening = () =>
   console.log(`🟢 Server listenting on port http://localhost:${PORT} 🎈`);
