00e429df6415751428536bcd77239280f777f9f4
YuJin Kim <kyuj0810@gmail.com>
2022-07-17 23:09:26 +0900

    video un/mute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -10,18 +10,18 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
-};
-const handelPause = (e) => {
-  playBtn.innerText = "Play";
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handelPlay = (e) => {
-  playBtn.innerText = "Pause";
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
 };
-
-const handleMute = (e) => {};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handelPause);
-video.addEventListener("play", handelPlay);
