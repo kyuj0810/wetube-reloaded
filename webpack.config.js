8011fd157efe6ae05d42d39c8b4f2475df9fc1d6
jin <kyuj0810@gmail.com>
2022-08-22 23:16:29 +0900

    recorder setup, video preview

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
 
 module.exports = {
   entry: {
-    main: "./src/client/js/main.js",
-    videoPlayer: "./src/client/js/videoPlayer.js",
+    main: './src/client/js/main.js',
+    videoPlayer: './src/client/js/videoPlayer.js',
+    recorder: './src/client/js/recorder.js',
   },
-  mode: "development",
+  mode: 'development',
   watch: true,
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "css/styles.css",
+      filename: 'css/styles.css',
     }),
   ],
   output: {
-    filename: "js/[name].js",
-    path: path.resolve(__dirname, "assets"),
+    filename: 'js/[name].js',
+    path: path.resolve(__dirname, 'assets'),
     clean: true,
   },
   module: {
@@ -23,16 +24,16 @@ module.exports = {
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
           },
         },
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
-};
+}
