d7f5c8034b3743956390b0730ca84f3c7c9af5f0
jin <kyuj0810@gmail.com>
2022-09-12 19:10:48 +0900

    thumbnail upload part1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,64 +1,90 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
-
-const startBtn = document.getElementById('startBtn')
+/* 
+ffmpeg는 영상을 변환해서 오디오파일로 만들거나, 포맷을 변환헤주는 도구
+무겁고, 보통은 백엔드에서 사용하지만, webassembly덕분에 프론트엔드에서도 사용할 수 있게 됐음.
+*/
+const actionBtn = document.getElementById('actionBtn')
 const video = document.getElementById('preview')
 
 let stream
 let recorder
 let videoFile
 
+const files = {
+  input: 'recording.webm',
+  output: 'output.mp4',
+  thumb: 'thumbnail.jpg',
+}
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement('a')
+  a.href = fileUrl
+  a.download = fileName
+  document.body.appendChild(a)
+  a.click()
+}
+
 const handleDownload = async () => {
+  actionBtn.removeEventListener('click', handleDownload)
+  actionBtn.innerText = 'Transcoding...'
+  actionBtn.disabled = true
+
   const ffmpeg = createFFmpeg({ log: true })
   await ffmpeg.load()
 
-  ffmpeg.FS('writeFile', 'recording.webm', await fetchFile(videoFile)) // writeFile : ffmpeg의 가상의 세계에 파일을 생성해줌
+  ffmpeg.FS('writeFile', files.input, await fetchFile(videoFile)) // writeFile : ffmpeg의 가상의 세계에 파일을 생성해줌
 
-  await ffmpeg.run('-i', 'recording.webm', '-r', '60', 'output.mp4')
+  await ffmpeg.run('-i', files.input, '-r', '60', files.output)
 
   await ffmpeg.run(
     '-i',
-    'recording.webm',
+    files.input,
     '-ss',
     '00:00:01',
     '-frames:v',
     '1',
-    'thumbnail.jpg',
+    files.thumb,
   )
 
-  const mp4File = ffmpeg.FS('readFile', 'output.mp4')
-  const thumbFile = ffmpeg.FS('readFile', 'thumbnail.jpg')
+  const mp4File = ffmpeg.FS('readFile', files.output)
+  const thumbFile = ffmpeg.FS('readFile', files.thumb)
 
+  // binary data로 video, image 생성
   const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' })
   const thumbBlob = new Blob([thumbFile.buffer], { type: 'image/jpg' })
 
   const mp4Url = URL.createObjectURL(mp4Blob)
   const thumbUrl = URL.createObjectURL(thumbBlob)
 
-  const a = document.createElement('a')
-  a.href = mp4Url
-  a.download = 'MyRecording.mp4'
-  document.body.appendChild(a)
-  a.click()
+  downloadFile(mp4Url, 'MyRecording.mp4')
+  downloadFile(thumbUrl, 'MyThumbnail.jpg')
 
-  const thumbA = document.createElement('a')
-  thumbA.href = thumbUrl
-  thumbA.download = 'MyThumbnail.jpg'
-  document.body.appendChild(thumbA)
-  thumbA.click()
+  ffmpeg.FS('unlink', files.input)
+  ffmpeg.FS('unlink', files.output)
+  ffmpeg.FS('unlink', files.thumb)
+
+  //이 파일들을 메모리에서 삭제하기 위해 처리
+  URL.revokeObjectURL(mp4Url)
+  URL.revokeObjectURL(thumbUrl)
+  URL.revokeObjectURL(videoFile)
+
+  actionBtn.disabled = false
+  actionBtn.innerText = 'Record Again'
+  actionBtn.addEventListener('click', handleStart)
 }
 
 const handleStop = () => {
-  startBtn.innerText = 'Download Recording'
-  startBtn.removeEventListener('click', handleStop)
-  startBtn.addEventListener('click', handleDownload)
+  actionBtn.innerText = 'Download Recording'
+  actionBtn.removeEventListener('click', handleStop)
+  actionBtn.addEventListener('click', handleDownload)
 
   recorder.stop()
 }
 
 const handleStart = () => {
-  startBtn.innerText = 'Stop Recording'
-  startBtn.removeEventListener('click', handleStart)
-  startBtn.addEventListener('click', handleStop)
+  actionBtn.innerText = 'Stop Recording'
+  actionBtn.removeEventListener('click', handleStart)
+  actionBtn.addEventListener('click', handleStop)
   recorder = new MediaRecorder(stream, { mimeType: 'video/webm' })
   recorder.ondataavailable = (event) => {
     videoFile = URL.createObjectURL(event.data) // 파일은 브라우저의 메모리 상에 있음.
@@ -81,4 +107,4 @@ const init = async () => {
 
 init()
 
-startBtn.addEventListener('click', handleStart)
+actionBtn.addEventListener('click', handleStart)
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,8 +1,9 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trime: true, maxlength: 80 },
   fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
   description: { type: String, required: true, trime: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trime: true }],
@@ -10,14 +11,14 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, required: true, default: 0 },
     rating: { type: Number, required: true, default: 0 },
   },
-  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
-});
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+})
 
-videoSchema.static("formatHashtags", function (hashtags) {
+videoSchema.static('formatHashtags', function (hashtags) {
   return hashtags
-    .split(",")
-    .map((word) => (word.startsWith("#") ? word : `#${word}`));
-});
+    .split(',')
+    .map((word) => (word.startsWith('#') ? word : `#${word}`))
+})
 
 // videoSchema.pre("save", async function () {
 //   this.hashtags = this.hashtags[0]
@@ -25,5 +26,5 @@ videoSchema.static("formatHashtags", function (hashtags) {
 //     .map((word) => (word.startsWith("#") ? word : `#${word}`));
 // });
 
-const Video = mongoose.model("Video", videoSchema);
-export default Video;
+const Video = mongoose.model('Video', videoSchema)
+export default Video
