5d93288833183390f114b93198451e26b60a83b6
jin <kyuj0810@gmail.com>
2022-10-16 17:45:41 +0900

    commenting, rendering comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,9 @@
+import { async } from 'regenerator-runtime'
+
 const videoContainer = document.getElementById('videoContainer')
 const form = document.getElementById('commentForm')
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault()
   const textarea = form.querySelector('textarea')
   const text = textarea.value
@@ -9,13 +11,14 @@ const handleSubmit = (event) => {
   if (text === '') {
     return
   }
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json', // json middleware로 처리해야한다고 express에게 알려줘야함.
     },
     body: JSON.stringify({ text }),
   })
+  textarea.value = ''
 }
 
 if (form) {
--- src/controller/videoController.js
+++ src/controller/videoController.js
@@ -1,6 +1,7 @@
 import User from '../models/User'
 import Video from '../models/Video'
 import Comment from '../models/Comment'
+import { async } from 'regenerator-runtime'
 
 /* callback 형식
 console.log("start")
@@ -33,7 +34,8 @@ export const home = async (req, res) => {
 }
 export const watch = async (req, res) => {
   const { id } = req.params
-  const video = await Video.findById(id).populate('owner')
+  const video = await Video.findById(id).populate('owner').populate('comments')
+  console.log(video)
   if (!video) {
     return res.render('404', { pageTitle: 'Video not found.' })
   }
@@ -152,9 +154,22 @@ export const registerView = async (req, res) => {
   return res.sendStatus(200)
 }
 
-export const createComment = (req, res) => {
-  console.log(req.params)
-  console.log(req.body)
-  console.log(req.body.test, req.body.rating)
-  return res.sendStatus(200)
+export const createComment = async (req, res) => {
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req
+  const video = await Video.findById(id)
+  if (!video) {
+    return res.sendStatus(404)
+  }
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  })
+  video.comments.push(comment._id)
+  video.save()
+  return res.sendStatus(201)
 }
