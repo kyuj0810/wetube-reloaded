16aa5ee6fdc2dbf7979a7eee085d9addd5b8f07c
jin <kyuj0810@gmail.com>
2022-10-16 18:15:01 +0900

    realtime comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,6 +3,22 @@ import { async } from 'regenerator-runtime'
 const videoContainer = document.getElementById('videoContainer')
 const form = document.getElementById('commentForm')
 
+const addComment = (text) => {
+  const videoComments = document.querySelector('.video__comments ul')
+  const newComment = document.createElement('li')
+  newComment.className = 'video__comment'
+  const icon = document.createElement('i')
+  icon.className = 'fas fa-comment'
+  //console.log('icon:', icon)
+  const span = document.createElement('span')
+  span.innerText = ` ${text}`
+  //console.log('span:', span)
+  newComment.appendChild(icon)
+  newComment.appendChild(span)
+  //console.log(newComment)
+  videoComments.prepend(newComment)
+}
+
 const handleSubmit = async (event) => {
   event.preventDefault()
   const textarea = form.querySelector('textarea')
@@ -11,13 +27,16 @@ const handleSubmit = async (event) => {
   if (text === '') {
     return
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json', // json middleware로 처리해야한다고 express에게 알려줘야함.
     },
     body: JSON.stringify({ text }),
   })
+  if (status === 201) {
+    addComment(text)
+  }
   textarea.value = ''
 }
 
